9bc4f798659d64c799ea276d876e551e1fec36d8
liaoran201012 <142066976+liaoran201012@users.noreply.github.com>
2026-02-11 11:37:17 +0800

    Update script source URL in AdSlot component

--- src/components/AdSlot.jsx
+++ src/components/AdSlot.jsx
@@ -13,7 +13,7 @@ export default function AdSlot({ minHeight = 280 }) {
     const script = document.createElement('script');
     script.async = true;
     script.charset = 'utf-8';
-    script.src = 'https://tpwgt.com/content?currency=usd&trs=484712&shmarker=693317&show_hotels=true&powered_by=true&locale=en&searchUrl=www.aviasales.com%2Fsearch&primary_override=%2332a8dd&color_button=%2332a8dd&color_icons=%2332a8dd&dark=%23262626&light=%23FFFFFF&secondary=%23FFFFFF&special=%23C4C4C4&color_focused=%2332a8dd&border_radius=0&plain=false&promo_id=7879&campaign_id=100'';
+    script.src = 'https://tpwgt.com/content?currency=usd&trs=484712&shmarker=693317&show_hotels=true&powered_by=true&locale=en&searchUrl=www.aviasales.com%2Fsearch&primary_override=%2332a8dd&color_button=%2332a8dd&color_icons=%2332a8dd&dark=%23262626&light=%23FFFFFF&secondary=%23FFFFFF&special=%23C4C4C4&color_focused=%2332a8dd&border_radius=0&plain=false&promo_id=7879&campaign_id=100';
     el.appendChild(script);
 
     return () => {
